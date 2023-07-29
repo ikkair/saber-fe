b0c67df5c44d6bfc7df39ccbc3265a8e30b5d8c0
Septian-tri <Septiantriwidian@gmail.com>
2023-07-29 23:40:10 +0700

    [IMPROVE] simplifying syntax for set  circle image background

--- app/src/main/java/com/desti/saber/MainActivity.java
+++ app/src/main/java/com/desti/saber/MainActivity.java
@@ -3,32 +3,29 @@ package com.desti.saber;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.res.AssetManager;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
-import java.io.InputStream;
+import com.desti.saber.utils.ImageSetterFromStream;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AssetManager asm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.asm = getAssets();
+
+        ImageSetterFromStream imageSetter = new ImageSetterFromStream(this);
         Button signUpBtn = findViewById(R.id.signUpBtn);
         Button logInBtn = findViewById(R.id.logInBtn);
 
         //Looping for background image btn circle
         for (int i = 1; i <= 5; i++){
             String viewXmlId = "image_btn_" + i;
-            ImageView resourceView = findViewById(getResources().getIdentifier(viewXmlId, "id", getPackageName()));
-            setCircleButtonImage("img_btn_" + i + ".png", resourceView);
+            String imageName = "img_btn_" + i + ".png";
+            int resourceId = getResources().getIdentifier(viewXmlId, "id", getPackageName());
+            imageSetter.setAsImageDrawable(imageName, resourceId);
         }
 
         signUpBtn.setOnClickListener(new View.OnClickListener() {
@@ -37,31 +34,13 @@ public class MainActivity extends AppCompatActivity {
                 doSignUpBtnClick();
             }
         });
+
         logInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 doLogInBtnClick();
             }
         });
-    }
-
-    public void loginButtonHandler(View v){
-        Intent loginActivityIntent = new Intent(this, LoginActivity.class);
-        startActivity(loginActivityIntent);
-    }
-
-    public void registerButtonHandler(View v){
-        Intent registerActivityIntent = new Intent(this, RegisterActivity.class);
-        startActivity(registerActivityIntent);
-    }
-
-    private void setCircleButtonImage(String imageName, ImageView imv){
-        try {
-            InputStream ism = this.asm.open(imageName);
-            imv.setImageDrawable(Drawable.createFromStream(ism, null));
-        }catch (Exception err){
-            System.out.println(err);
-        }
     }
 
     private void doLogInBtnClick(){
