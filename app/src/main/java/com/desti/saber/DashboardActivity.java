0c6dc38845d818f14ed96eedb256fb968b322c75
Septian-tri <Septiantriwidian@gmail.com>
2023-07-31 23:03:39 +0700

    [FEATURE] ongoing dashboard ui slicing

--- app/src/main/java/com/desti/saber/DashboardActivity.java
+++ app/src/main/java/com/desti/saber/DashboardActivity.java
@@ -1,8 +1,11 @@
 package com.desti.saber;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.desti.saber.utils.ImageSetterFromStream;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,6 +13,8 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        ImageSetterFromStream isfs = new ImageSetterFromStream(this);
+        isfs.setAsImageDrawable("location_icon.png", R.id.locationIcon);
     }
 
 }
