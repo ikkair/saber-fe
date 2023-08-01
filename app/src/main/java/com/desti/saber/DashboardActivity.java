0ea15653fae54ad781f422e6a9ae2c6ffc60bf16
Septian-tri <Septiantriwidian@gmail.com>
2023-08-02 00:13:40 +0700

    ongoing dashboard

--- app/src/main/java/com/desti/saber/DashboardActivity.java
+++ app/src/main/java/com/desti/saber/DashboardActivity.java
@@ -1,7 +1,14 @@
 package com.desti.saber;
 
+import android.graphics.Outline;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewOutlineProvider;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +21,19 @@ public class DashboardActivity extends AppCompatActivity {
        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         ImageSetterFromStream isfs = new ImageSetterFromStream(this);
+        FrameLayout iam = findViewById(R.id.iam);
+
+        iam.setClipToOutline(true);
+        iam.setTranslationZ(20);
+        iam.setTranslationX(0);
+        iam.setElevation(30);
+        iam.setOutlineProvider(new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                Rect rect = new Rect(0, 20, view.getWidth(), view.getHeight());
+                outline.setRoundRect(rect, 20);
+            }
+        });
         isfs.setAsImageDrawable("location_icon.png", R.id.locationIcon);
     }
 
