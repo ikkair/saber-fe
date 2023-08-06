09f9ffcd32f6e4af1bbd898a2acc599e98f8896c
Septian-tri <Septiantriwidian@gmail.com>
2023-08-06 20:42:36 +0700

    [FEATURE] added dto class for logIn

---
+++ app/src/main/java/com/desti/saber/utils/dto/DataLogInDTO.java
@@ -0,0 +1,85 @@
+package com.desti.saber.utils.dto;
+
+public class DataLogInDTO {
+     private String id;
+     private String name;
+     private String role;
+     private String email;
+     private String phone;
+     private String photo;
+     private Long balance;
+     private String token;
+     private String refreshToken;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Long balance) {
+        this.balance = balance;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
