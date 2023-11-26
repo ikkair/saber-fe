5c8706e6f5cd9d180ec95576fd2967ec6e478db3
ikkair <ikhlasul.kamal.i@gmail.com>
2023-11-26 10:19:59 +0700

    change from volley to okhttp

--- app/src/main/java/com/desti/saber/MainActivity.java
+++ app/src/main/java/com/desti/saber/MainActivity.java
@@ -2,7 +2,9 @@ package com.desti.saber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,11 +12,16 @@ import android.widget.Button;
 import com.desti.saber.utils.ImageSetterFromStream;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Initialize shared preferences
+        sharedPreferences = getSharedPreferences("UserData", Context.MODE_PRIVATE);
+
+
 
         ImageSetterFromStream imageSetter = new ImageSetterFromStream(this);
         Button signUpBtn = findViewById(R.id.signUpBtn);
